73c001b0cc2553ba6f49e5d55245caad8d1262be
sarmistha-jena <sarmistha_jena@apple.com>
2025-11-13 20:14:24 +0530

    Fix 2 SG

--- Task14-Modules/modules/network_security/main.tf
+++ Task14-Modules/modules/network_security/main.tf
@@ -64,15 +64,15 @@ resource "aws_security_group" "public_http_sg" {
   }
 }
 
-resource "aws_security_group_rule" "allow_alb_to_instances" {
-  type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.public_http_sg.id
-  security_group_id        = aws_security_group.public_http_sg.id
-  description              = "Allow HTTP traffic within same SG (ALB to EC2)"
-}
+# resource "aws_security_group_rule" "allow_alb_to_instances" {
+#   type                     = "ingress"
+#   from_port                = 80
+#   to_port                  = 80
+#   protocol                 = "tcp"
+#   source_security_group_id = aws_security_group.public_http_sg.id
+#   security_group_id        = aws_security_group.public_http_sg.id
+#   description              = "Allow HTTP traffic within same SG (ALB to EC2)"
+# }
 
 resource "aws_security_group" "private_http_sg" {
   name        = var.private_http_security_group_name
